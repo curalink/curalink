838f166c7b0b3cd705117f6fcfef94b0163ac54e
MaciekTalaska <maciek.talaska@gmail.com>
2014-12-12 23:54:22 +0100

    normalizing white spaces

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,25 +1,25 @@
 class LinksController < ApplicationController
-    def new
-      @link = Link.new
-    end
+  def new
+    @link = Link.new
+  end
 
-    def create
-        @link = Link.new(link_params)
-	
-	if @link.save
-	  redirect_to @link
-	else
-	  render 'new'
-	end
-    end
+  def create
+    @link = Link.new(link_params)
 
-    def show
-        @link = Link.find(params[:id])
-    end
+  	if @link.save
+  	  redirect_to @link
+  	else
+  	  render 'new'
+  	end
+  end
 
-    def index
-        @links = Link.all
-    end
+  def show
+      @link = Link.find(params[:id])
+  end
+
+  def index
+      @links = Link.all
+  end
 private
   def link_params
     params.require(:link).permit(:title, :uri)
