9a4e7984fc4358a3e3f35014dff8d245aa44c1aa
MaciekTalaska <maciek.talaska@gmail.com>
2016-02-11 22:56:39 +0100

    reference between author & links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 require 'valid_url'
 
 class Link < ActiveRecord::Base
+  belongs_to :author
   validates :title, presence: true,
 		    length: { minimum: 5 }
   validates :uri, presence: true, :url => true
