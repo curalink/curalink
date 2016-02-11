9c493a58f9f4a6d63eeabee9f3058fc4af226015
MaciekTalaska <maciek.talaska@gmail.com>
2016-02-11 22:55:31 +0100

    author added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211214231) do
+ActiveRecord::Schema.define(version: 20160211215133) do
+
+  create_table "authors", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "links", force: true do |t|
     t.string   "title"
