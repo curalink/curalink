199230ad7eb1e6b7587ca5c387642193c6ce93b1
MaciekTalaska <maciek.talaska@gmail.com>
2016-02-21 18:43:00 +0100

    minor changes to routing & menu

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'front_page#index'
+  root 'links#index'
   resources :links
 
   # Example of regular route:
